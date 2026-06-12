dad19d0256d0670b5f9bc4e019dd0295a77bdc3c
Giuliano Sohrobigarat <gino.sohro@yahoo.com.ar>
2026-06-12 17:23:16 -0300

    Sprint 1 - Configuracion inicial Nest y MongoDB

---
+++ backend/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+
+@Module({
+  imports: [
+  ConfigModule.forRoot({
+    isGlobal: true,
+  }),
+
+  MongooseModule.forRoot(
+    'mongodb://ginosohro_db_user:ItGoDvWx3S6hJDU9@ac-lpkqasd-shard-00-00.ezznrwd.mongodb.net:27017,ac-lpkqasd-shard-00-01.ezznrwd.mongodb.net:27017,ac-lpkqasd-shard-00-02.ezznrwd.mongodb.net:27017/red-social?ssl=true&replicaSet=atlas-efhl8v-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0',
+  ),
+
+  AuthModule,
+  UsersModule,
+  PostsModule,
+],
+})
+export class AppModule {}
