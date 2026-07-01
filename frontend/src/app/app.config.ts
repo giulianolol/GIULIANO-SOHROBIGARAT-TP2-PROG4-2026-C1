ceb57f91186ec4b0d16ac81f6cade72df3715ed2
Giuliano Sohrobigarat <gino.sohro@yahoo.com.ar>
2026-07-01 09:53:09 -0300

    PWA

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,20 +1,13 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-} from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 
 import { provideRouter } from '@angular/router';
 
-import {
-  provideHttpClient,
-  withInterceptors,
-} from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
-import {
-  authInterceptor,
-} from './core/interceptors/auth.interceptor';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -22,10 +15,10 @@ export const appConfig: ApplicationConfig = {
 
     provideRouter(routes),
 
-    provideHttpClient(
-      withInterceptors([
-        authInterceptor,
-      ]),
-    ),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
